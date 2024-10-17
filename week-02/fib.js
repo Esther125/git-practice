1afae9f780b6537f4abc09cf187496e08a2c3107
Esther125 <friday50523@gmail.com>
2024-10-18 00:31:58 +0800

    fix: add another implement for fib

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,19 +1,33 @@
+// Method 1: Use Recursion
 function fib(n) {
     // TODO: implement fibonacci
-    if (n==0) {
-        return 0
-    }else if(n==1){
-        return 1 
-    } else if (n==2){
-        return 1
-    }
-    else {
-        return fib(n-1)+fib(n-2)
+    if (n == 0) {
+        return 0;
+    } else if (n == 1 || n == 2) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
     }
 }
-  
-console.log(fib(0)); // 0
-console.log(fib(1)); // 1
-console.log(fib(5)); // 5
-console.log(fib(10)); // 55
-console.log(fib(15)) // 610
+
+// Method 2: Use for loop
+function fib_2(n) {
+    let arr = [1, 1];
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    for (let i = 3; i <= n; i++) {
+        let tmp = arr[i - 3] + arr[i - 2];
+        arr.push(tmp);
+    }
+    return arr[n - 1];
+}
+
+console.log(fib_2(0)); // 0
+console.log(fib_2(1)); // 1
+console.log(fib_2(5)); // 5
+console.log(fib_2(10)); // 55
+console.log(fib_2(15)); // 610
